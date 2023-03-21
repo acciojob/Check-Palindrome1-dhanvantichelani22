6c36198f412b54a8b93f5f19cdb32d05a96ff8a0
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-03-21 23:16:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-
+  var palin = str.toLowerCase();
+	let i=0,j=i-1;
+	while(i<j){
+	// palin = str.charAt[i==j]
+		if(palin.charAt(i)==palin.charAt(j)){
+			i++;
+			j--;
+		}else {
+			return false;
+		}
+		
+	}
+	return true;
 }
 module.exports = palindrome
